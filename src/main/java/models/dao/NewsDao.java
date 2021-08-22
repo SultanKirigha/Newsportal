5aa33dce6c5dcacb0985b6463efba86f3084a6fd
“SultanMwangoyaKirigha” <sultan.kirigha@student.moringaschool.com>
2021-08-22 20:23:56 +0300

    made newsdao file

--- src/main/java/models/dao/NewsDao.java
+++ src/main/java/models/dao/NewsDao.java
@@ -1,4 +1,23 @@
 package models.dao;
 
-public class NewsDao {
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+    //create
+    void add (News news);
+    // void addNewsToDepartment(News news, Department department)
+
+    //read
+    List<News> getAll();
+    News findById(int id);
+    // List<Department> getAllDepartmentForANews(int NewsId);
+
+    //update
+    void update(int id, String employee, String role, String depertment, String news);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
