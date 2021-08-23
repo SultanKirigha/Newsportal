0503e8c018d2534ab2895f010f32b64857755ece
“SultanMwangoyaKirigha” <sultan.kirigha@student.moringaschool.com>
2021-08-23 04:36:29 +0300

    made some changes

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,22 @@
 package dao;
 
-public class NewsDao {
+import models.News;
+import java.util.List;
+
+public interface NewsDao {
+    //create
+    void add(News news);
+    void addNewsToDepartment(int deptid, int newsid, int userid);
+
+    //read
+    News findById(int id);
+    List<News> allNews();
+    List<News> allGeneralNews();
+    List<News> allDepartmentalNews();
+
+    //update
+
+    //delete
+    void deleteById(int id);
+    void deleteAll();
 }
